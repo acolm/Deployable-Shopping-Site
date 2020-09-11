2c6caf7ea5d42f4e4d7cc33d3c78b6f756429983
acolm <aaronc3000@gmail.com>
2020-09-11 05:37:56 -0700

    made nessessary files for it to run

--- application/app.js
+++ application/app.js
@@ -7,6 +7,11 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use((req, res, next) => {
+    console.info('\x1b[42m\x1b[30m Request URL : ' + req.url + '\x1b[0m');
+    next();
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,5 +19,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500);
+    console.log(err);
+    res.sendFile('error.html', {root: "public/html" });
+})
 
 module.exports = app;
