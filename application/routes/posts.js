bc67796769ac8f550646087a70ae5e66de3e11fe
MooOfDoom <lotharnarins@hotmail.com>
2020-11-04 22:30:02 -0800

    Add PriceMin and PriceMax filters to marketplace

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -125,4 +125,46 @@ router.get('/BIGsearch/:searchTerm/:searchCategory/:searchType/:searchClass', (r
     })
     .catch((err) => next(err));
 });
+
+router.get('/fullsearch/:searchTerm/:searchCategory/:searchType/:searchClass/:searchPriceMin/:searchPriceMax', (req, resp, next) => {
+    let searchTerm = req.params.searchTerm;
+    let searchCategory = req.params.searchCategory;
+    let searchType = req.params.searchType;
+    let searchClass = req.params.searchClass;
+    let searchPriceMin = req.params.searchPriceMin;
+    let searchPriceMax = req.params.searchPriceMax;
+    
+    let arguments = [];
+    let _sql = 'SELECT p.id, p.title, p.price, p.thumbnail, p.created, p.type \
+    FROM posts p WHERE ';
+    if (searchTerm !== '__NO_VALUE__') {
+        _sql += 'title LIKE ? AND ';
+        arguments.push('%' + searchTerm + '%');
+    }
+    if(searchCategory !== 'All'){
+        _sql += 'category = ? AND ';
+        arguments.push(searchCategory);
+    }
+    if(searchType !== 'All'){
+        _sql += 'type = ? AND ';
+        arguments.push(searchType);
+        check[2] = 1;
+    }
+    if(searchClass !== '__NO_VALUE__'){
+        _sql += 'class LIKE ? AND ';
+        arguments.push('%' + searchClass + '%');
+    }
+    if(searchPriceMin !== '__NO_VALUE__'){
+        _sql += 'price >= ' + searchPriceMin + ' AND ';
+    }
+    if(searchPriceMax !== '__NO_VALUE__'){
+        _sql += 'price <= ' + searchPriceMax + ' AND ';
+    }
+    _sql +='approved = 1;';
+    db.query(_sql, arguments)
+    .then(([results, fields]) => {
+        resp.json(results);
+    })
+    .catch((err) => next(err));
+});
 module.exports = router;
