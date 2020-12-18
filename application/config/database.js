777f5547f5d6ad1acd0fcc1dd6ea2b7e75c83c5d
Kevin J Nunura <knunura@mail.sfsu.edu>
2020-12-17 16:09:01 -0800

    Committing and pushign before switching to another branch. Only modified spacing in profile pic.

--- application/config/database.js
+++ application/config/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host:"localhost" ,
-    user:"photoapp",
-    password:"CSC317",
+    user:"root",
+    password:"53905390",
     database:"gatormartdb",
     connectionLimit:50,
     queueLimit: 0,
